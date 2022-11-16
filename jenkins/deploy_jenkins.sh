99a076af300e77bb4ae6d1d28dcc68ab6103d697
bm935325 <bm935325@head.bmv.secureedu-pg0.clemson.cloudlab.us>
2022-11-15 20:19:55 -0500

    set env for user,Kubehead in values

--- jenkins/deploy_jenkins.sh
+++ jenkins/deploy_jenkins.sh
@@ -7,6 +7,8 @@ helm repo update
 
 export KUBEHEAD=$(kubectl get nodes -o custom-columns=NAME:.status.addresses[1].address,IP:.status.addresses[0].address | grep head | awk -F ' ' '{print $2}')
 cp /users/$USER/csc603-project/jenkins/values.yaml .
-sed -i "s/KUBEHEAD/${KUBEHEAD}/g" values.yaml
+sed -i "s/REPLACEME_USER/${USER}/g" values.yaml
+sed -i "s/REPLACEME_KUBEHEAD/${KUBEHEAD}/g" values.yaml
+
 helm install -f values.yaml jenkins/jenkins --generate-name
 
